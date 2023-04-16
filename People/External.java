1687489eb6be452b2af84a86c58cc51d00164567
Erick-Chin <90728024+Erick-Chin@users.noreply.github.com>
2023-04-16 15:17:00 -0500

    member class!

--- People/External.java
+++ People/External.java
@@ -2,5 +2,4 @@ package People;
 
 public class External extends Member{
     protected int id;
-    protected
 }
--- People/Member.java
+++ People/Member.java
@@ -1,7 +1,7 @@
 package People;
 
 public class Member extends Person {
-    protected int id;//id for the memeber
+    protected int id;//id for the member
     protected int numOfBooks;//how many books they have checked out
     //protected float balance; //every person should have a balance
 
@@ -18,5 +18,29 @@ public class Member extends Person {
         super.setEmail(p.getEmail());
         super.setSSN(p.getSSN());
         super.setDate(p.getDate());
+        super.setBalance(p.getBalance());
+    }
+    public void setID(int id){
+        this.id=id;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public void setNumOfBooks(int numOfBooks){
+        this.numOfBooks=numOfBooks;
+    }
+
+    public float getNumOfBooks(){
+        return numOfBooks;
+    }
+    public void printInfo(){
+        System.out.println("Name: "+ getName());
+        System.out.println("Address: "+ getAddress());
+        System.out.println("Email: "+ getEmail());
+        System.out.println("SSN: "+ getSSN1());
+        System.out.println("Dob: "+ getDate1());
+        System.out.println("Balance:"+ getBalance());
     }
 }
