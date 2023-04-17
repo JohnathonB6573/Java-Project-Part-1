8f5cfee8884e38a02498921478469154950b68ec
Johnathon Brumfield <91100117+JohnathonB6573@users.noreply.github.com>
2023-04-17 18:21:11 -0500

    fix

--- Collections/Collection.java
+++ Collections/Collection.java
@@ -4,27 +4,36 @@ import Other.Date;
 
 public class Collection {
 
-    protected String idNumber;
+    //protected String idNumber;
     protected String title;
     protected Date publicationDate;
     protected String section, author;
     protected double price;
 
-
-    public void setID(String id) {
-    	try {
-			if(id.length()==6) {
-				this.idNumber = id;
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+    //Default Constructor
+    Collection(){
+        this.title = " ";
+        this.publicationDate = new Date("00/00/0000");
+        this.section = " ";
+        this.author = " ";
+        this.price = 0;
     }
 
-    public String getID() {
-        return this.idNumber;
-    }
+    //public void setID(String id) {
+    //	try {
+	//		if(id.length()==6) {
+	//			this.idNumber = id;
+	//		}
+	//	} catch(Exception e) {
+	//		e.printStackTrace();
+	//	}
+    //}
 
+    //public String getID() {
+       // return this.idNumber;
+    //}
+
+    //Getters
     public String getTitle(){
         return this.title;
     }
@@ -45,8 +54,30 @@ public class Collection {
         return this.price;
     }
 
+    //Setters
+
+    public void setPrice(double d){
+        this.price = d;
+    }
+
+    public void setAuthor(String s){
+        this.author = s;
+    }
+
+    public void setSection(String s){
+        this.section = s;
+    }
+
+    public void setPublicationDate(Date d){
+        this.publicationDate = d;
+    }
+
+    public void setTitle(String s){
+        this.title = s;
+    }
+
     public void saveTo(String file)
     {
-
+        //idk what this is for
     }
 }
