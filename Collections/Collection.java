bf42ad0edf9d43d78edc1019b1c1a3f0cadb4498
Ty Hastings <hastingsty44@gmail.com>
2023-04-17 13:22:29 -0500

    Added get/set methods

--- Collections/Collection.java
+++ Collections/Collection.java
@@ -12,19 +12,39 @@ public abstract class Collection {
         return this.idNumber;
     }
 
+    public void setID(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
     public Date getPubDate() {
         return this.publicationDate;
+    }
+
+    public void setDate(Date d) {
+        this.publicationDate = d;
     }
 
     public String getSection() {
         return this.section;
     }
 
+    public void setSection(String section) {
+        this.section = section;
+    }
+
     public String getAuthor() {
         return this.author;
     }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public float getPrice() {
         return this.price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
     }
 }
