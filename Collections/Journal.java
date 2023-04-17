c417b373ce49bf8d7c9de88146d944547e24dd98
wes <wes32315@gmail.com>
2023-04-17 16:33:55 -0500

    update Collections

--- Collections/Journal.java
+++ Collections/Journal.java
@@ -1,5 +1,30 @@
 package Collections;
+import Other.*;
 //Journal has an ISSN number
 public class Journal extends Collection{
+    protected String ISSN;
+
+    Journal(){
+        this.idNumber = 0;
+        this.publicationDate = new Date("00/00/0000");
+        this.section = "";
+        this.author = "";
+        this.price = 0.0;
+    }
+
+    Journal(int idNumber, String ISSN, String title, String publicationDate, String section, String author, Double price)
+    {
+        this.idNumber = idNumber;
+        this.ISSN = ISSN;
+        this.title = title;
+        this.publicationDate = new Date(publicationDate);
+        this.section = section;
+        this.author = author;
+        this.price = price;
+    }
+
+    String getISSN(){
+        return this.ISSN;
+    }
     
 }
--- Collections/Newspaper.java
+++ Collections/Newspaper.java
@@ -1,5 +1,30 @@
 package Collections;
+import Other.*;
 //Newspaper has an ISSN number
 public class Newspaper extends Collection{
+    protected String ISSN;
+
+    Newspaper(){
+        this.idNumber = 0;
+        this.publicationDate = new Date("00/00/0000");
+        this.section = "";
+        this.author = "";
+        this.price = 0.0;
+    }
+
+    Newspaper(int idNumber, String ISSN, String title, String publicationDate, String section, String author, Double price)
+    {
+        this.idNumber = idNumber;
+        this.ISSN = ISSN;
+        this.title = title;
+        this.publicationDate = new Date(publicationDate);
+        this.section = section;
+        this.author = author;
+        this.price = price;
+    }
+
+    String getISSN(){
+        return this.ISSN;
+    }
     
 }
