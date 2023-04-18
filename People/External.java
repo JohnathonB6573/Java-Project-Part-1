c03c09fb2d12446f9e63a88d6151ea612f487809
Erick-Chin <90728024+Erick-Chin@users.noreply.github.com>
2023-04-17 20:02:12 -0500

    external

--- People/External.java
+++ People/External.java
@@ -1,5 +1,15 @@
 package People;
 
 public class External extends Member{
-    protected int id;
+    public External(Member m) {
+        super.setName(m.getName());
+        super.setAddress(m.getAddress());
+        super.setEmail(m.getEmail());
+        super.setSSN(m.getSSN());
+        super.setDate(m.getDate());
+        super.setBalance(m.getBalance());
+        super.setID(m.getID());
+        super.setNumOfBooks(m.getNumOfBooks());
+    }
+
 }
