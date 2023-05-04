0d49f981b145905fef49318d593e32a8c7e817dc
Erick-Chin <90728024+Erick-Chin@users.noreply.github.com>
2023-05-04 17:01:05 -0500

    restarting the fx shit

--- FxLibarySystem/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.fxlibarysystem {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.fxlibarysystem to javafx.fxml;
-    exports com.example.fxlibarysystem;
-}
