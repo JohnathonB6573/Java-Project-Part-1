fc7d8e554d905ec68646872833dbfd4d39ea463d
Erick-Chin <90728024+Erick-Chin@users.noreply.github.com>
2023-05-04 19:51:10 -0500

    Button changes

--- LibarySystem/src/main/java/com/example/libarysystem/HelloController.java
+++ LibarySystem/src/main/java/com/example/libarysystem/HelloController.java
@@ -1,14 +1,26 @@
 package com.example.libarysystem;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onExitButtonStage(Stage exitStage)throws IOException {
+        exitStage.close();
     }
 }
